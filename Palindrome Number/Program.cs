5d073b94fa65e6474a5ea73c364f2f3485689fe5
mohammedatefa <mohamed.atef.abdelsbor@gmail.com>
2024-01-10 12:42:32 +0200

    solve the proplem of the IsPalindrome Given an integer x, return true if x is a palindrome , and false otherwise.

---
+++ Palindrome Number/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Palindrome_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+        }
+        public static bool IsPalindrome(int x)
+        {
+            string cloneNumber = x.ToString();
+            string reverseNum = "";
+            int start = 0;
+            for (int i = cloneNumber.Length - 1; i >= 0; i--)
+            {
+                reverseNum += cloneNumber[i];
+                start++;
+            }
+
+            return reverseNum == cloneNumber;
+        }
+    }
+
+}
