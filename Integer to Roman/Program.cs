e89c7ab8031c1d9b255d0dd4b3edb85ef0413692
mohammedatefa <mohamed.atef.abdelsbor@gmail.com>
2024-01-10 13:31:25 +0200

    Integer to Roman problem that you have an Integer number and you should reprsent it in Roman format for Example 25 => XXV 3=>III

---
+++ Integer to Roman/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+
+namespace Integer_to_Roman
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"the number is 3 in roman => {IntToRoman(25)}");
+            Console.WriteLine($"the number is 58 in roman => {IntToRoman(58)}");
+            Console.WriteLine($"the number is 400 in roman => {IntToRoman(400)}");
+            Console.WriteLine($"the number is 900 in roman => {IntToRoman(900)}");
+            Console.WriteLine($"the number is 1994 in roman => {IntToRoman(1994)}");
+       
+        }
+
+        public static string IntToRoman(int num)
+        {
+            string romanString = "";
+            Dictionary<int,string> romanLetters = new Dictionary<int, string> {
+                {1000,"M" },
+                {900,"CM" },
+                {500,"D"},
+                {400,"CD"},
+                {100,"C" },
+                {90,"XC" },
+                {50,"L" },
+                {40,"XL"},
+                {10,"X" },
+                {9,"IX" },
+                {5,"V" },
+                {4,"IV" },
+                {1,"I"}
+            };
+
+            foreach (int i in romanLetters.Keys)
+            {
+                while (i<=num)
+                {
+                    romanString += romanLetters[i];
+                    num -= i;
+                }
+            }
+            return romanString;
+        }
+    }
+}
--- Palindrome Number/Program.cs
+++ Palindrome Number/Program.cs
@@ -4,8 +4,15 @@
     {
         static void Main(string[] args)
         {
+            int test1 = 121;
+            Console.WriteLine(IsPalindrome(test1)); //output => True
 
-           
+            int test2 = -121;
+            Console.WriteLine(IsPalindrome(test1)); //output => False 
+
+            int test3 = 10;
+            Console.WriteLine(IsPalindrome(test1)); //output => False 
+
         }
         public static bool IsPalindrome(int x)
         {
